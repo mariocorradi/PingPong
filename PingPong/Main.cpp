ca5a6959c583e603442242b338a21b24f6e015d6
DESKTOP-TT94HGH\mario <mariocorradi91@gmail.com>
2017-02-25 12:10:20 +0100

    update

--- PingPong/Main.cpp
+++ PingPong/Main.cpp
@@ -1,12 +1,19 @@
 #include "Game.h"
+#include <memory>
 
 int main(int argc, char* args[])
 {
 	 Game g(800, 640);
-	while (g.Run())
+	 auto gameLoop = std::make_shared<Game>(800,600);
+	
+	 //puntatore
+	 
+	 gameLoop->Run();
+
+	 /*while (g.Run())
 	{
 
-	}
+	}*/
 	
 	return 0;
 }
